ec5218264132f2984cfda5a65c5ea64c11a2dd1c
Tony Camero <tonycamerobiz@gmail.com>
2026-01-26 09:08:08 -0800

    chore: remove local debug artifacts

--- backend/test-pdfkit.js
+++
@@ -1,7 +0,0 @@
-try {
-    const pdf = require('pdfkit');
-    console.log('Success resolving pdfkit');
-} catch (e) {
-    console.error('Failed to resolve:', e.message);
-    console.error('Require stack:', e.requireStack);
-}
