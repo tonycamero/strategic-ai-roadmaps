cb3853e965efa002de6395cf1ce4648508648502
Tony Camero <tonycamerobiz@gmail.com>
2026-02-11 17:55:44 -0800

    Fix(netlify): remove accidental .ts.ts extensions (zisi resolver)

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import * as authController from '../controllers/auth.controller.ts.ts';
+import * as authController from '../controllers/auth.controller.ts';
 
 const router = Router();
 
